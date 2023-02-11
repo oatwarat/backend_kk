828cd124d1380a896164a206cb2d669064523a00
nammonman <sirapat69@outlook.com>
2023-02-11 13:45:48 +0700

    aaa

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_header=["*"],
+    allow_headers=["*"],
 )
 
 class Device(BaseModel):
